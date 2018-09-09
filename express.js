d0f48a794346d7363e6b346085bdc88169f2a308
Grzegorz Stosio <0mcabra@gmail.com>
2018-09-09 21:33:23 +0200

    hosing JSON file

--- express.js
+++ express.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
+let data = fs.readFileSync('products.json') || [];
+let parsedData = JSON.parse(data);
+const express = require('express');
+const app = express();
 
-const express = require('express')
-const app = express()
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('/', (req, res) => res.send(JSON.stringify({a:2})))
+app.get('/', (req, res) => res.send(JSON.stringify(parsedData)))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
