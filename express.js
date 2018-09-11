6d3dcb4de2abe8f9c1392b6084d22570255833f8
Grzegorz Stosio <0mcabra@gmail.com>
2018-09-11 14:22:01 +0200

    changed api for products

--- express.js
+++ express.js
@@ -4,12 +4,13 @@ let parsedData = JSON.parse(data);
 const express = require('express');
 const app = express();
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('/', (req, res) => res.send(JSON.stringify(parsedData)))
+app.get('/', (req, res) => res.send(JSON.stringify({test: "elo elo 3 2 0"})))
+
+app.get('/products', (req, res) => res.send(JSON.stringify(parsedData)))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
